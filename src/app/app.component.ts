16ab016eebf2c157125518176ea70b4f99fbda9c
teaRain9 <adel.ht@gmail.com>
2022-06-03 16:31:24 -0700

    secong commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'text-editor-quill';
 
+  editorForm!: FormGroup;
+
+
+  ngOnInit() {
+
+    this.editorForm.addControl('quill', new FormControl(''))
+
+  }
 
 }
